9a479dd41c6606c8a34e0f352e564c493f78e6f2
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2025-03-04 05:43:25 +0800

    User session handling implementation and bug fixes

--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,17 @@
-from flask import render_template, request,make_response
+from flask import render_template, request, make_response, session, redirect, url_for
 from app import app
 from app.model import *
 import time
 import threading
+import os
+from datetime import datetime, timedelta
+
+TIMEOUT_DURATION = timedelta(seconds=20) # Session timeout time
+
+# Session settings
+app.config['SECRET_KEY'] = os.urandom(12)  # Secure random key
+app.config['PERMANENT_SESSION_LIFETIME'] = TIMEOUT_DURATION # Session timeout
+app.config['SESSION_TYPE'] = 'filesystem'
 
 registeredAccsAndTimes = []
 timeout = []
@@ -28,8 +37,32 @@ def timerEnd(username,rem):
     if rem:
         timeout.remove(username)
 
+@app.before_request
+def check_session_timeout():
+    # Check if logged in
+    if 'username' in session:
+        # Get last activity time
+        last_activity_str = session.get('last_activity')
+        if last_activity_str:
+            last_activity = datetime.strptime(last_activity_str, '%Y-%m-%d %H:%M:%S')
+            # Check if session has expired
+            if datetime.now() - last_activity > TIMEOUT_DURATION:
+                session.clear()
+                return render_template('old-user.html', error_message = "Invalid username or password!")
+                return render_template('old-user.html', error_message = "Session Timeout, Please Log in again")
+            else:
+                # Update activity time
+                session['last_activity'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            # If there's no last activity time, set one
+            session['last_activity'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 @app.route('/')
 def index():
+    name = session.get('username')
+    if name:  # If logged in, redirect to dashboard
+        return redirect('/dashboard.html')
+    
     return render_template('budget-tracker.html')
 
 @app.route('/submitRegister', methods=['POST'])
@@ -51,9 +84,17 @@ def submit():
     if flag == 1:
         insertNewUser(idnum, username, firstName, lastName, email, password, phoneNumber)
         return render_template('old-user.html', username=username)
+    if flag == 0:
+        error_message = "Passwords don't match!"
+    if flag == -1:
+        error_message = "Username in use!"
+    if flag == -2:
+        error_message = "Email in use!"
+    if flag == -3:
+        error_message = "Phone number in use!"
     
     return render_template('/new-user.html',email=email, firstName=firstName, lastName=lastName,
-                           phoneNumber=phoneNumber,username=username, flag = flag)
+                           phoneNumber=phoneNumber,username=username, error_message = error_message)
 
 @app.route('/old-user.html')
 def loginPage():
@@ -72,18 +113,13 @@ def login():
     global registeredAccsAndTimes
     [username,isAdmin] = checkLogin(request.form['username'],request.form['password'])
     print(isAdmin)
-    if isAdmin == 0 and username not in timeout: # USER LOGIN
-        response = make_response(render_template('/dashboard.html'))
-        response.set_cookie('un',str(username))
-        response.set_cookie('iA',str(isAdmin))
-        timerEnd(username,0)
-        return response
-    elif isAdmin >= 1 and username not in timeout: # ADMIN LOGIN
-        response = make_response(render_template('/chief.html'))
-        response.set_cookie('un',str(username))
-        response.set_cookie('iA',str(isAdmin))
-        timerEnd(username,0)
-        return response
+    if username not in timeout: # LOGIN
+        session.permanent = True  # Permanent session (session exist after browser closing)
+        session['username'] = username
+        session['isAdmin'] = str(isAdmin)
+        session['last_activity'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timerEnd(username, 0)
+        return redirect('/dashboard.html')
 
     if not any(pair[0] == username for pair in registeredAccsAndTimes):
         registeredAccsAndTimes.append([username,0])
@@ -97,39 +133,53 @@ def login():
                 stop_event, thread = start_background_timer(username,1)
                 
     print(registeredAccsAndTimes)
-    return render_template('old-user.html', loginFail=True, username=username)
+    return render_template('old-user.html', username=username, error_message = "Invalid username or password!")
 
 @app.route('/saveProfile', methods=['POST'])
 def saveProfile():
     picture = request.files['file']
-    #filename = secure_filename(picture.filename)
-    filename = picture.filename
     bytePicture = picture.read()
-    username = request.cookies.get('un')
-    isAdmin = request.cookies.get('iA')
-    if isAdmin=='0':
-        if saveToDB(bytePicture, username): # USER PROFPIC CHANGES
-            return render_template('dashboard.html')
-        return render_template('profile.html')
-    else:
-        if saveToDB(bytePicture, username): # ADMIN PROFPIC CHANGES
-            return render_template('chief.html')
-        return render_template('chief.html')
+    username = session.get('username')
+    if saveToDB(bytePicture, username): # USER PROFPIC CHANGES
+        return render_template('dashboard.html')
+    return render_template('profile.html')
 
 @app.route('/dashboard.html')
-def parentDashboard():
-    return render_template('dashboard.html')
+def dashboard():
+    isAdmin = session.get('isAdmin')
+    if not session.get('username'):  # Check if logged in
+        return redirect('/old-user.html')
+    
+    if isAdmin == '0':
+        return render_template('dashboard.html')
+    elif isAdmin == '1':
+        return redirect('chief.html')
 
 @app.route('/profile.html')
 def profile():
-    name=request.cookies.get('un')
+    name = session.get('username')
+    isAdmin = session.get('isAdmin')
+    
+    if not name:  # If not logged in, redirect to login page
+        return redirect('/old-user.html')
+    
+    if isAdmin=='0':
+        role = "User"
+    elif isAdmin == '1':
+        role = "Admin"
+        
     [user,email,data] = retrieveData(name)
     if data==0:
-        return render_template('profile.html',username=user,email=email,image=data, defaultHidden="", profHidden="hidden")
-    return render_template('profile.html',username=user,email=email,image=data, defaultHidden="hidden", profHidden="")
+        return render_template('profile.html',username=user,email=email,image=data, defaultHidden="", profHidden="hidden", role = role)
+    return render_template('profile.html',username=user,email=email,image=data, defaultHidden="hidden", profHidden="", role = role)
 
 @app.route('/settings.html')
 def settings():
+    name = session.get('username')
+    
+    if not name:  # If not logged in, redirect to login page
+        return redirect('/old-user.html')
+    
     return render_template('settings.html')
 
 '''
@@ -144,14 +194,19 @@ def childProfile():
 
 @app.route('/logout')
 def logout():
-    response = make_response(render_template('/budget-tracker.html'))
-    for cookie in request.cookies:
-        response.delete_cookie(cookie)
-    return response
+    session.clear()
+    return make_response(render_template('/budget-tracker.html'))
 
-@app.route('/chief')
+@app.route('/chief.html')
 def admin():
-    return render_template('chief.html')
+    name = session.get('username')
+
+    if not name:  # If not logged in, redirect to login page
+        return redirect('/old-user.html')
+    [user,email,data] = retrieveData(name)
+    if data==0:
+        return render_template('chief.html', image=data, defaultHidden="", profHidden="hidden")
+    return render_template('chief.html', image=data, defaultHidden="hidden", profHidden="")
 
 @app.errorhandler(404)
 def notFound(e):
