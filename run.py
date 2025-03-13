f9760db33707f61626b4388b2d9a7d19538f941a
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2025-03-14 03:26:13 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,5 +8,4 @@ from app import app
 DEBUG_FLAG = False # For detailed errors. False in production
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=5000, ssl_context='adhoc', debug=DEBUG_FLAG)
-    app.run(debug=DEBUG_FLAG)
+    app.run(host='0.0.0.0', port=5000, ssl_context=("certs/cert.pem", "certs/key.pem"), debug=DEBUG_FLAG)
